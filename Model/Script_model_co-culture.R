f69ad556f70efb3fd39dd7ad68175412fbb1f7f6
Pham Khiem <122158508+KhiemPham00@users.noreply.github.com>
2023-03-17 18:45:53 +0700

    Update Script_model_co-culture.R

--- Model/Script_model_co-culture.R
+++ Model/Script_model_co-culture.R
@@ -8,8 +8,8 @@ sybil::SYBIL_SETTINGS("SOLVER","glpkAPI")
 
 #read input file
 #medium <-read.csv("E:/Đề tài/GEM đi thi/gapseq-master/dat/media/TSBmed.csv")
-pa <- readRDS("E:/Đề tài/GEM đi thi/pa.RDS") #directory file in your computer
-sa <- readRDS("E:/Đề tài/GEM đi thi/sa.RDS") #directory file in your computer
+pa <- readRDS(".../pa.RDS") #directory file in your computer
+sa <- readRDS(".../sa.RDS") #directory file in your computer
 
 #from data to bacterial
 bacpa <- Bac(pa)
@@ -21,7 +21,7 @@ arena = Arena(n=100, m=100, Lx=0.025, Ly=0.025)
 arena <- addOrg(arena, bacpa, amount=1)
 arena <- addOrg(arena, bacsa, amount=1)
 
-arena_subs <- fread("E:/Đề tài/GEM đi thi/gapseq-master/dat/media/TSBmed.csv")
+arena_subs <- fread(".../TSBmed.csv")
 arena_subs[, ex.rxn := paste0("EX_", compounds, "_e0")]
 arena <- addSubs(arena, smax = arena_subs$maxFlux, 
                  mediac = arena_subs$ex.rxn, unit = "mM", addAnyway = T)
