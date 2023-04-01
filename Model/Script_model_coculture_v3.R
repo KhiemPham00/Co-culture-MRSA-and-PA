10505e0358ebc4668e4edcbc3edf19e08b8e1ffa
Pham Khiem <122158508+KhiemPham00@users.noreply.github.com>
2023-04-01 20:37:20 +0700

    Update Script_model_coculture_v3.R

--- Model/Script_model_coculture_v3.R
+++ Model/Script_model_coculture_v3.R
@@ -1,15 +1,15 @@
 #install package BacArena, sybil, glpkAPI, data.table
 
+#load library
 library(BacArena)
 library(sybil)
 library(glpkAPI)
 library(data.table)
 sybil::SYBIL_SETTINGS("SOLVER","glpkAPI")
 
-#read input file
-#medium <-read.csv("E:/Đề tài/GEM đi thi/gapseq-master/dat/media/TSBmed.csv")
-pa <- readRDS("C:/Khiem/Model BacArena/pa.RDS") #directory file in your computer
-sa <- readRDS("C:/Khiem/Model BacArena/sa.RDS") #directory file in your computer
+#input genome metabolic model
+pa <- readRDS("/pa.RDS") #directory file in your computer
+sa <- readRDS("/sa.RDS") #directory file in your computer
 
 #from data to bacterial
 bacpa <- Bac(pa, type="Pseudomonas aeruginosa", chem = "EX_cpd00007_e0")
@@ -18,10 +18,11 @@ bacsa <- Bac(sa, type="MRSA")
 #setting enviroment culture
 arena = Arena(n=100, m=100, Lx=0.025, Ly=0.025)
 
+#add bacteria
 arena <- addOrg(arena, bacpa, amount=15)
 arena <- addOrg(arena, bacsa, amount=85)
 
-arena_subs <- fread("C:/Khiem/Model BacArena/TSBmed.csv")
+arena_subs <- fread("/TSBmed.csv") #dỉrectory
 arena_subs[, ex.rxn := paste0("EX_", compounds, "_e0")]
 arena <- addSubs(arena, smax = arena_subs$maxFlux, 
                  mediac = arena_subs$ex.rxn, unit = "mM", addAnyway = T)
@@ -29,17 +30,13 @@ arena <- createGradient(arena,smax=75,mediac=c("EX_cpd00007_e0"),
                     position='top',steep=0.5, add=TRUE)
 chemotaxis(bacpa,arena,1, "EX_cpd00007_e0", arena@occupyM)
 
-
 #start simulation
-sim <- simEnv(arena,time=18)
+sim <- simEnv(arena,time=24)
 
-par(mfrow=c(1,1))
+#show result
 evalArena(sim, plot_items = "Population", phencol = F, time = seq(0,18,1), 
           show_legend = FALSE, legend_pos = "bottom")
 
-
 plotCurves2(sim, legendpos = "right")
 
-''
-#show graph
-
+#end
